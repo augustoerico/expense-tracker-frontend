1df5f0b57fc7ed5b0ad8a128f0b3112f1923d941
Erico Silva <augusto.ericosilva@gmail.com>
2017-04-02 17:13:13 -0300

    Refactor auth form; add form to sign up component

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -6,17 +6,33 @@ var AuthForm = React.createClass({
         
         var username = this.refs.username.value.trim();
         var password = this.refs.password.value;
+        var accountType = this.refs.accountType.value;
 
         if (username && password) {
-            this.props.onSubmit(username, password);
+            this.props.onSubmit(username, password, accountType);
         } 
     },
     render: function () {
+        var {showAccountTypeSelector} = this.props;
+        
+        var renderAccountTypeSelector = () => {
+            if (showAccountTypeSelector) {
+                return (
+                    <select refs="accountType">
+                        <option value="admin">Admin</option>
+                        <option value="regular">Regular</option>
+                    </select>
+                );
+            }
+            return;
+        };
+        
         return (
             <div>
                 <form onSubmit={this.onSubmit}>
                     <input ref="username" type="text" placeholder="username" />
                     <input ref="password" type="password" placeholder="password" />
+                    {renderAccountTypeSelector()}
                     <button className="button" type="submit">Submit</button>
                 </form>
             </div>
--- app/components/SignUp.jsx
+++ app/components/SignUp.jsx
@@ -1,10 +1,16 @@
 var React = require('react');
 
+var AuthForm = require('AuthForm');
+
 var SignUp = React.createClass({
+    onSubmit: function (username, password, accountType) {
+        alert('Ahoy! ' + username + ' ' + password + ' ' + accountType);
+    },
     render: function () {
         return (
             <div>
                 SignUp.jsx
+                <AuthForm showAccountTypeSelector={true} onSubmit={this.onSubmit}/>
             </div>
         );
     }
